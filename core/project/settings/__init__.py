5207eac35e1bbc8f2c3ede25276ea1cffbc952af
sifesife7 <sifesife7@gmail.com>
2024-04-04 12:50:46 +0200

    project configuration

---
+++ core/project/settings/__init__.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+from split_settings.tools import include
+
+#Decouple
+from decouple import Csv,config
+from dotmap import DotMap
+
+#Init environmant
+ENV = DotMap({'config': config, 'Csv': Csv})
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+ADMIN_PATH = ENV.config('ADMIN_PATH')
+
+#Debug
+DEBUG = ENV.config('DEBUG', cast=bool)
+
+from split_settings.tools import include
+
+include(
+    'base.py',
+    'utils_config.py'
+)
+
+if DEBUG:
+    include ('development.py')
+else:
+    include ('production.py')
---
+++ core/project/settings/base.py
@@ -0,0 +1,71 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = NotImplemented
+
+# SECURITY WARNING: don't run with debug turned on in production!
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'core.project.urls'
+
+
+
+WSGI_APPLICATION = 'core.project.wsgi.application'
+
+
+
+# Password validation
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
